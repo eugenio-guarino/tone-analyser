9f6c1109cb4f8012b0842fb322375ad7e6e72ae4
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2020-01-19 22:21:30 +0000

    Add Watson Tone Analyser service

---
+++ ToneAnalyserFunction/IBMWatsonToneAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Watson.ToneAnalyzer.v3;
+using IBM.Watson.ToneAnalyzer.v3.Model;
+
+
+namespace ToneAnalyserFunction
+{
+    class IBMWatsonToneAnalyzer : SentimentAnalysisService
+    {
+        public override string ApiKey { get; set; }
+        public override string ApiEndpoint { get; set; }
+
+        public override string AnalyseText(string text)
+        {
+            IamAuthenticator authenticator = new IamAuthenticator(apikey: ApiKey);
+
+            ToneAnalyzerService toneAnalyzer = new ToneAnalyzerService("2017-09-21", authenticator);
+            toneAnalyzer.SetServiceUrl(ApiEndpoint);
+
+            ToneInput toneInput = new ToneInput()
+            {
+                Text = text
+            };
+
+            var result = toneAnalyzer.Tone(
+                toneInput: toneInput
+                );
+
+            return result.Response;
+        }
+    }
+}
---
+++ ToneAnalyserFunction/TextAnalyser.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ToneAnalyserFunction
+{
+    public static class TextAnalyser
+    {
+        [FunctionName("TextAnalyser")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string service = req.Query["service"];
+            string text = req.Query["text"];
+            string apiKey = req.Query["apiKey"];
+            string apiEndpoint = req.Query["apiEndpoint"];
+
+            SentimentAnalysisService textAnalyticsService = null;
+
+            if (service == "TextAnalytics")
+            {
+
+            }
+            else if (service == "WatsonAnalyser")
+            {
+                textAnalyticsService = new IBMWatsonToneAnalyzer
+                {
+                    ApiKey = apiKey,
+                    ApiEndpoint = apiEndpoint
+                };
+            }
+
+            string requestBody = textAnalyticsService.AnalyseText(text);
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            return data != null
+                ? (ActionResult)new OkObjectResult($"{data}")
+                : new BadRequestObjectResult("Choose either 'WatsonAnalyser' or 'TextAnalytics' for the service query string value.");
+        }
+    }
+}
