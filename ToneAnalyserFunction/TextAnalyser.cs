12823b7822a7133b43711fb2e3e3ee7c9ac4c43d
eugenio-guarino <eugenio.guarino@students.plymouth.ac.uk>
2020-01-22 21:53:37 +0000

    Add logs and send response to IoT hb

--- ToneAnalyserFunction/TextAnalyser.cs
+++ ToneAnalyserFunction/TextAnalyser.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.Devices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
+using System.Text;
 
 namespace ToneAnalyserFunction
 {
@@ -24,11 +26,15 @@ namespace ToneAnalyserFunction
             const string MicrosoftSentimentAnalysisApiKey = "";
             const string MicrosoftSentimentAnalysisApiEndpoint = "";
 
+            const string deviceName = "MyNodeDevice";
+
             string serviceSelected = req.Query["service"];
 
             // Do speech-to-text conversion
             SpeechCognitiveService speechService = new SpeechCognitiveService(speechSubscriptionKey, speechServiceRegion);
             string textToAnalyse = await speechService.RecognizeSpeechAsync(req.Body, log);
+
+            log.LogInformation("Speech-To-Text result: " + textToAnalyse);
 
             // Choose which text analytics service to use
             TextAnalyticsService textAnalyticsService;
@@ -45,12 +51,25 @@ namespace ToneAnalyserFunction
                 return new BadRequestObjectResult("You need to pick service");
             }
 
-            string requestBody = textAnalyticsService.AnalyseText(textToAnalyse);
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string result = textAnalyticsService.AnalyseText(textToAnalyse);
 
-            return data != null
-                ? (ActionResult)new OkObjectResult($"{data}")
-                : new BadRequestObjectResult("Whoops, something went wrong!");
+            log.LogInformation("Analysis result: " + result);
+
+            // Send translation result as C2D message
+            string iothubConnectionString = System.Environment.GetEnvironmentVariable("iotHubConnectionString");
+            var serviceClient = ServiceClient.CreateFromConnectionString(iothubConnectionString);
+            var commandMessage = new Message(Encoding.ASCII.GetBytes(result));
+            await serviceClient.SendAsync(deviceName, commandMessage);
+
+
+            if (result != null)
+            {
+                return new OkObjectResult("Analysis result: " + result);
+            }
+            else
+            {
+                return new BadRequestObjectResult("Failed to analyze speech.");
+            }
         }
     }
 }
